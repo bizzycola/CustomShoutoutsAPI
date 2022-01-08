008648a3549a5685d258de394e357f1e7b417479
Bizzycola <furbanj@gmail.com>
2022-01-09 01:13:15 +1100

    Get unread notifs, mark as read

--- CustomShoutoutsAPI/GraphQL/Mutations/UserMutations.cs
+++ CustomShoutoutsAPI/GraphQL/Mutations/UserMutations.cs
@@ -12,6 +12,36 @@ namespace CustomShoutoutsAPI.GraphQL.Mutations
     [ExtendObjectType("Mutation")]
     public class UserMutations
     {
+        [Auth]
+        public async Task<bool> MarkNotificationsRead([Service] IHttpContextAccessor http, Guid[] ids)
+        {
+            if (http.HttpContext == null)
+                throw new Exception("HTTP Context not loaded");
+
+            var uid = (string?)http.HttpContext.Items["userId"];
+            if (uid == null) throw new Exception("Not authenticated");
+
+            var scope = http.HttpContext.RequestServices.CreateScope();
+            var ctx = scope.ServiceProvider.GetRequiredService<DataContext>();
+
+            var errors = new List<string>();
+            foreach (var id in ids)
+            {
+                try
+                {
+                    var obj = await ctx.UserNotifications.FirstOrDefaultAsync(p => p.Id == id && p.ForId == uid);
+                    if (obj == null) continue;
+
+                    obj.Read = true;
+                    obj.ReadAt = DateTime.UtcNow;
+                }
+                catch { }
+            }
+
+            await ctx.SaveChangesAsync();
+            return true;
+        }
+
         [GraphQLDescription("Update the default shoutout message for the authenticated user")]
         [Auth]
         public async Task<bool> UpdateDefaultSO([Service] IHttpContextAccessor http, string so)
--- CustomShoutoutsAPI/GraphQL/Queries/UserQueries.cs
+++ CustomShoutoutsAPI/GraphQL/Queries/UserQueries.cs
@@ -12,6 +12,24 @@ namespace CustomShoutoutsAPI.GraphQL.Queries
     [ExtendObjectType("Query")]
     public class UserQueries
     {
+        [Auth]
+        public async Task<List<UserNotification>> GetUnreadNotifications([Service] IHttpContextAccessor http)
+        {
+            if (http.HttpContext == null)
+                throw new Exception("HTTP Context not loaded");
+
+            var uid = (string?)http.HttpContext.Items["userId"];
+            if (uid == null) throw new Exception("Not authenticated");
+
+            var scope = http.HttpContext.RequestServices.CreateScope();
+            var ctx = scope.ServiceProvider.GetRequiredService<DataContext>();
+
+            return await ctx.UserNotifications
+                .Where(p => p.ForId == uid && !p.Read)
+                .OrderByDescending(p => p.Created)
+                .ToListAsync();
+        }
+
         [GraphQLDescription("Return the default shoutout set on the authenticated account")]
         [Auth]
         public async Task<string> GetDefaultSO([Service] IHttpContextAccessor http)
